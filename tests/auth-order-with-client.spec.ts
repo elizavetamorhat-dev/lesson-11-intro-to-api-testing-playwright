79c33fdbfb1ec4a351e7eb67cbe53cb9408f2d85
AndrewKhv <44403470+AndrewKhv@users.noreply.github.com>
2025-10-16 20:48:46 +0300

    feat: TL-12 prettier fixes

--- tests/auth-order-with-client.spec.ts
+++ tests/auth-order-with-client.spec.ts
@@ -2,16 +2,16 @@ import { expect, test } from '@playwright/test'
 import { ApiClient } from '../src/ApiClient'
 
 test('login and create order with api client', async ({ request }) => {
-  const apiClient = await ApiClient.create(request);
-  const orderId = await apiClient.createOrderAndReturnOrderId();
-  console.log('orderId:', orderId);
-});
+  const apiClient = await ApiClient.create(request)
+  const orderId = await apiClient.createOrderAndReturnOrderId()
+  console.log('orderId:', orderId)
+})
 
 test('get orders with api client', async ({ request }) => {
-  const apiClient = await ApiClient.create(request);
-  const ordersBefore = await apiClient.getOrders();
-  await apiClient.createOrderAndReturnOrderId();
-  const ordersAfter = await apiClient.getOrders();
+  const apiClient = await ApiClient.create(request)
+  const ordersBefore = await apiClient.getOrders()
+  await apiClient.createOrderAndReturnOrderId()
+  const ordersAfter = await apiClient.getOrders()
 
-  expect(ordersBefore.length < ordersAfter.length).toBeTruthy();
-});
+  expect(ordersBefore.length < ordersAfter.length).toBeTruthy()
+})
